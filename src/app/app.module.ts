65e5fa99a97323d358b4ca278b29004ad7eda4cf
Jesse Mykolyn <jrmykolyn@gmail.com>
2019-06-21 19:37:50 -0400

    Import and inject LoginContainer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginContainerComponent } from './ui/containers/login-container/login-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginContainerComponent
   ],
   imports: [
     BrowserModule
