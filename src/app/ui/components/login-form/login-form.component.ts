0d1395091d0b58e0ed9eb07aaccf7d4137c5fcba
Jesse Mykolyn <jrmykolyn@gmail.com>
2019-06-22 21:07:40 -0400

    Update LoginForm to read from/write to cache

--- src/app/ui/components/login-form/login-form.component.ts
+++ src/app/ui/components/login-form/login-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { CacheService } from '../../../../services/cache/cache.service';
+
 
 @Component({
   selector: 'app-login-form',
@@ -6,20 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent implements OnInit {
-  data: { username: string, password: string } = { username: '', password: '' }
+  id: string = 'LoginFormComponent'
 
-  constructor() {
+  data: { username: string, password: string }
+
+  constructor(
+    private cache: CacheService,
+    private el: ElementRef
+  ) {
+    if (this.cache.has(this.id)) {
+      this.data = this.cache.get(this.id);
+    } else {
+      this.data = { username: '', password: '' };
+    }
+
+    // Expose LoginFormComponent for debugging purposes.
+    (window as any).__FORM__ = this;
   }
 
   ngOnInit() {
+    this.el.nativeElement.querySelector('#username').value = this.data.username;
+    this.el.nativeElement.querySelector('#password').value = this.data.password;
   }
 
   updateUsername(e) {
     this.data.username = e.target.value;
+
+    // Cache current username.
+    this.cache.add(this.id, this.data);
   }
 
   updatePassword(e) {
     this.data.password = e.target.value;
+
+    // Cache current password.
+    this.cache.add(this.id, this.data);
   }
 
   isValid(data): boolean {
@@ -45,6 +68,9 @@ export class LoginFormComponent implements OnInit {
 
     if (this.isValid(this.data)) {
       console.log('__ DATA IS VALID');
+
+      // Invalidate cached form data.
+      this.cache.delete(this.id);
     } else {
       console.log('__ DATA IS NOT VALID');
     }
