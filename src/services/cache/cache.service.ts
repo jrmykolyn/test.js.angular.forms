a36eea64d64f0dbf61a0a4729144931352762d43
Jesse Mykolyn <jrmykolyn@gmail.com>
2019-06-22 21:06:40 -0400

    Define CacheService API

--- src/services/cache/cache.service.ts
+++ src/services/cache/cache.service.ts
@@ -4,6 +4,25 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CacheService {
+  window: { [key: string]: any } = window
 
-  constructor() { }
+  constructor() {
+  }
+
+  add(key, value) {
+    this.window.localStorage.setItem(key, JSON.stringify({ data: value }));
+  }
+
+  get(key) {
+    const { data = null } = JSON.parse(this.window.localStorage.getItem(key)) || {};
+    return data;
+  }
+
+  has(key) {
+    return !!this.get(key);
+  }
+
+  delete(key) {
+    this.window.localStorage.removeItem(key);
+  }
 }
