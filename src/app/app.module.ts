5af54c7b57bd1160175e4dcce852e3ef31a73bea
Jesse Mykolyn <jrmykolyn@gmail.com>
2019-06-23 21:56:41 -0400

    Add FormsModule and ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginContainerComponent } from './ui/containers/login-container/login-container.component';
@@ -12,7 +13,9 @@ import { LoginFormComponent } from './ui/components/login-form/login-form.compon
     LoginFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
