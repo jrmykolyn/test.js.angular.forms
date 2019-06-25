c1754580c420bd9f7b527357c7ce9e80a5296dd6
Jesse Mykolyn <jrmykolyn@gmail.com>
2019-06-24 22:02:57 -0400

    Replace ElementRef with FormControl

--- src/app/ui/components/login-form/login-form.component.ts
+++ src/app/ui/components/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { CacheService } from '../../../../services/cache/cache.service';
 
 
@@ -7,42 +8,21 @@ import { CacheService } from '../../../../services/cache/cache.service';
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
 })
-export class LoginFormComponent implements OnInit {
+export class LoginFormComponent {
   id: string = 'LoginFormComponent'
 
-  data: { username: string, password: string }
+  form: any
 
   constructor(
     private cache: CacheService,
-    private el: ElementRef
   ) {
-    if (this.cache.has(this.id)) {
-      this.data = this.cache.get(this.id);
-    } else {
-      this.data = { username: '', password: '' };
-    }
+    this.form = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
 
     // Expose LoginFormComponent for debugging purposes.
     (window as any).__FORM__ = this;
-  }
-
-  ngOnInit() {
-    this.el.nativeElement.querySelector('#username').value = this.data.username;
-    this.el.nativeElement.querySelector('#password').value = this.data.password;
-  }
-
-  updateUsername(e) {
-    this.data.username = e.target.value;
-
-    // Cache current username.
-    this.cache.add(this.id, this.data);
-  }
-
-  updatePassword(e) {
-    this.data.password = e.target.value;
-
-    // Cache current password.
-    this.cache.add(this.id, this.data);
   }
 
   isValid(data): boolean {
@@ -66,7 +46,10 @@ export class LoginFormComponent implements OnInit {
   onSubmit(e) {
     e.preventDefault();
 
-    if (this.isValid(this.data)) {
+    const data = Object.keys(this.form.controls)
+      .reduce((acc, key) => ({ ...acc, [key]: this.form.controls[key].value }), {});
+
+    if (this.isValid(data)) {
       console.log('__ DATA IS VALID');
 
       // Invalidate cached form data.
